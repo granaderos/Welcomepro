34484bcfafcb782bf886c142feb19f0fd789cd3d
granaderos <marejeanperpinosa@gmail.com>
2018-10-21 18:33:50 +0800

    Adding the entire project.

---
+++ index.php
@@ -0,0 +1,63 @@
+<html xmlns="http://www.w3.org/1999/html">
+    <head>
+        <title>I love Programming</title>
+        <link rel = "stylesheet" href = "CSS/bootstrap.min.css" />
+        <link rel = "stylesheet" href = "CSS/index.css" />
+        <script src="JS/jquery-1.9.1.min.js"></script>
+        <script src="JS/jquery-ui-1.10.2.min.js"></script>
+        <script src="JS/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div id = "header_container_div" class = "container-fluid">
+            <div class = "container">
+                <h1>Programming <3!</h1>
+            </div>
+            <div id = "navigation_container" class = "container">
+                <ul>
+                    <li><a href = ""> </a></li>
+                    <li><a href = ""> </a></li>
+                    <li><a href = ""> </a></li>
+                    <li><a href = ""> </a></li>
+                </ul>
+            </div><!-- navigation container ends-->
+        </div><!-- head container  div ends-->
+        <div id = "main_container_div" class = "container">
+            <p>Welcome back to the world of <span class = "label label-info">Software Development</span> Marejean!
+                It's almost been a year when I've been parted in my most favorite hobby - <span class = "label label-info">PROGRAMMING</span>. </p>
+
+            <div id = "fam_div">
+                <h3>Special Acknowledgement to:</h3>
+                <div id="myCarousel" class="carousel slide">
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                        <li data-target="#myCarousel" data-slide-to="4"></li>
+                        <li data-target="#myCarousel" data-slide-to="5"></li>
+                        <li data-target="#myCarousel" data-slide-to="6"></li>
+                        <li data-target="#myCarousel" data-slide-to="7"></li>
+                        <li data-target="#myCarousel" data-slide-to="8"></li>
+                        <li data-target="#myCarousel" data-slide-to="9"></li>
+                    </ol>
+                    <!-- Carousel items -->
+                    <div id='slide_imgs' class="carousel-inner">
+                        <div class="active item"><img src='CSS/images/maam&sir.jpg' class ='slideimg' alt='first'/></div>
+                        <div class="item"><img src='CSS/images/threeg.jpg' class ='slideimg' alt='second'/></div>
+                        <div class="item"><img src='CSS/images/threeg2.jpg' class ='slideimg' alt='third'/></div>
+                        <div class="item"><img src='CSS/images/threeg3.jpg' class ='slideimg' alt='fourth'/></div>
+                        <div class="item"><img src='CSS/images/threeg4.jpg' class = 'slideimg' alt='fifth'/></div>
+                        <div class="item"><img src='CSS/images/judy.jpg' class = 'slideimg' alt='sixth'/></div>
+                        <div class="item"><img src='CSS/images/atekim.jpg' class = 'slideimg' alt='seventh'/></div>
+                        <div class="item"><img src='CSS/images/three5.jpg' class = 'slideimg' alt='eight'/></div>
+                        <div class="item"><img src='CSS/images/maam.jpg' class = 'slideimg' alt='ninth'/></div>
+                        <div class="item"><img src='CSS/images/mac.jpg' class = 'slideimg' alt='tenth'/></div>
+                    </div>
+                    <!-- Carousel nav -->
+                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+                </div><!--Carousel slide show -->
+            </div>
+        </div><!-- end of container -->
+    </body>
+</html>
